598855ee84f3796bc5bb79878f437678c0a47854
Ivan Sosniuk <i.sosniuk@ukma.edu.ua>
2025-06-12 12:57:59 +0300

    Update 2025-06-12

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd C:/Users/user/Documents/Academia/Alg_data_struct/Practice/deploy/Spudei-mon-Deploy
+#cd C:/Users/user/Documents/Academia/Alg_data_struct/Practice/deploy/Spudei-mon-Deploy
 git pull origin main
 java -jar Spudei-mon.jar
 echo "Press Enter to exit"
